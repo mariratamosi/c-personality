57c3e51c19e3b89fd358541b42d56e7e8ab1268b
marira <rongrazzo@gmail.com>
2022-08-11 20:58:36 +0600

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 import PTRoute from "./routes/ptRoute"
+import Nav from "./components/Nav"
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <PTRoute />
     </div>
   )
---
+++ src/components/Nav.tsx
@@ -0,0 +1,14 @@
+function Nav() {
+  return (
+    <div className="nav">
+      <div>
+        <a href="/">logo</a>
+      </div>
+      <div>
+        <a href="/take-test">Take test</a>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
