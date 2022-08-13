fdcf6b621a9294097042ea7426db8a06f226f72b
marira <rongrazzo@gmail.com>
2022-08-13 16:04:58 +0600

    Nav design

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,22 @@
+import "../styles/components/nav.scss"
+import logo from "../assets/logo.png"
+
 function Nav() {
   return (
-    <div className="nav">
+    <div className="pt-nav">
       <div>
-        <a href="/">logo</a>
+        <a href="/" className="pt-logo-text">
+          <img src={logo} alt="pt logo" className="nav-logo" />
+          <span className="nav-brand-name">
+            <span>C-</span>
+            <span>Personality</span>
+          </span>
+        </a>
       </div>
       <div>
-        <a href="/take-test">Take test</a>
+        <a href="/take-test" className="cp-btn cp-large-btn">
+          Take the test
+        </a>
       </div>
     </div>
   )
