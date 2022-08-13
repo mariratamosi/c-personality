01306ac11a96d4f641a00414c435837126b026bc
marira <rongrazzo@gmail.com>
2022-08-13 19:28:17 +0600

    netlify redirect issue

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react"
 import "../styles/components/testPage.scss"
 
 function TestPage() {
+  useEffect(() => {
+    /*
+    Query logic
+    */
+    console.log("i fire once")
+  }, [])
   return <div className="test pt-page">Test</div>
 }
 
