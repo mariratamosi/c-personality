902825a805d59e360500dee9b7394fbb7e8f40db
marira <rongrazzo@gmail.com>
2022-08-13 18:58:47 +0600

    add home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,25 @@
 import "../styles/components/home.scss"
 
 function Home() {
-  return <div className="home pt-page">Home</div>
+  return (
+    <div className="home pt-page">
+      <div className="hero">
+        <div className="hero-text">
+          <h1>“It’s so incredible to finally be understood.”</h1>
+          <p>
+            Take our Personality Test and get a “freakishly accurate”
+            description of who you are and why you do things the way you do.
+          </p>
+        </div>
+        <div className="hero-action">
+          <a href="/take-test" className="cp-btn cp-x-large-btn">
+            Take the test
+          </a>
+          <div className="btn-sub-text">Only takes 10 minutes</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import "../styles/components/nav.scss"
-import logo from "../assets/logo.png"
+import logo from "../assets/logo.svg"
 
 function Nav() {
   return (
